a12149339c0d6856e75312c8b3fb935402529535
Joycee-Alfred-Nirmal <145605767+Joycee-Alfred-Nirmal@users.noreply.github.com>
2023-10-30 21:14:43 +0530

    created About component

--- react-portfolio/my-app/src/components/Navbar.jsx
+++ react-portfolio/my-app/src/components/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
         <ul className='hidden md:flex'>
           <li>Home</li>
           <li>About</li>
+          <li>Skills</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>

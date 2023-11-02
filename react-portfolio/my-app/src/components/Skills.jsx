c5168106d2bd4a52e107f713b60d63baeb219659
Joycee-Alfred-Nirmal <145605767+Joycee-Alfred-Nirmal@users.noreply.github.com>
2023-11-03 00:28:35 +0530

    added skills icons

--- react-portfolio/my-app/src/components/Skills.jsx
+++ react-portfolio/my-app/src/components/Skills.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import java from "../assets/java.png"
+import java from "../assets/java.png";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import bootstrap from "../assets/bootstrap.png";
+import angular from "../assets/angular.png";
+import spring from "../assets/spring.png";
+import springboot from "../assets/springboot.png";
+import mysql from "../assets/mysql.png";
+import mongodb from "../assets/mongodb.png";
+
 
 const Skills = () => {
   return (
@@ -10,13 +20,86 @@ const Skills = () => {
                 <p className='py-4'>//These are the technologies I've worked with</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 px-auto' src={java} alt='java.png'/>
 
                 </div>
 
             </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={html} alt='html.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={css} alt='css.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={javascript} alt='javascript.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={bootstrap} alt='bootstrap.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={angular} alt='angular.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={spring} alt='spring.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={springboot} alt='springboot.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={mysql} alt='mysql.png'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 px-auto' src={mongodb} alt='mongodb.png'/>
+
+                </div>
+
+            </div>
+
 
         </div>
         

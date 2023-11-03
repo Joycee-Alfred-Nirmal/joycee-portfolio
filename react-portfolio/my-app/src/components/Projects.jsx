ef6eda730802ba589fe08c172d6e7fe2ff3e2571
Joycee-Alfred-Nirmal <145605767+Joycee-Alfred-Nirmal@users.noreply.github.com>
2023-11-03 17:31:01 +0530

    created projects component

---
+++ react-portfolio/my-app/src/components/Projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600'>Projects</p>
+            <p className='py-6'> //Check out some of my recent works</p>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Projects
--- react-portfolio/my-app/src/components/Skills.jsx
+++ react-portfolio/my-app/src/components/Skills.jsx
@@ -13,92 +13,112 @@ import mongodb from "../assets/mongodb.png";
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 '>Skills </p>
                 <p className='py-4'>//These are the technologies I've worked with</p>
             </div>
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={java} alt='java.png'/>
+                    <img className='w-20 mx-auto' src={java} alt='java.png'/>
+                    <p className='my-4'>JAVA</p>
 
                 </div>
 
-            </div>
+        
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+         
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={html} alt='html.png'/>
+                    <img className='w-20 mx-auto' src={html} alt='html.png'/>
+                    <p className='my-4'>HTML</p>
+
 
                 </div>
 
-            </div>
+       
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+        
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={css} alt='css.png'/>
+                    <img className='w-20 mx-auto' src={css} alt='css.png'/>
+                    <p className='my-4'>CSS</p>
+
 
                 </div>
 
-            </div>
+          
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+          
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={javascript} alt='javascript.png'/>
+                    <img className='w-20 mx-auto' src={javascript} alt='javascript.png'/>
+                    <p className='my-4'>Javascript</p>
+
 
                 </div>
 
-            </div>
+        
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+           
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={bootstrap} alt='bootstrap.png'/>
+                    <img className='w-20 mx-auto' src={bootstrap} alt='bootstrap.png'/>
+                    <p className='my-4'>Bootstrap</p>
+
 
                 </div>
 
-            </div>
+          
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+          
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={angular} alt='angular.png'/>
+                    <img className='w-20 mx-auto' src={angular} alt='angular.png'/>
+                    <p className='my-4'>Angular</p>
+
 
                 </div>
 
-            </div>
+          
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+         
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={spring} alt='spring.png'/>
+                    <img className='w-20 mx-auto' src={spring} alt='spring.png'/>
+                    <p className='my-4'>Spring</p>
+
 
                 </div>
 
-            </div>
+      
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+          
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={springboot} alt='springboot.png'/>
+                    <img className='w-20 mx-auto' src={springboot} alt='springboot.png'/>
+                    <p className='my-4'>Spring Boot</p>
+
 
                 </div>
 
-            </div>
+           
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+          
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={mysql} alt='mysql.png'/>
+                    <img className='w-20 mx-auto' src={mysql} alt='mysql.png'/>
+                    <p className='my-4'>MySql</p>
+
 
                 </div>
 
-            </div>
+           
 
-            <div className='w-full grid grid-cols-1 sm:grid-cols-4 gap-4 text-center py-8'>
+          
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 px-auto' src={mongodb} alt='mongodb.png'/>
+                    <img className='w-20 mx-auto' src={mongodb} alt='mongodb.png'/>
+                    <p className='my-4'>MongoDb</p>
+
 
                 </div>
 
-            </div>
+</div>
+           
 
 
         </div>
